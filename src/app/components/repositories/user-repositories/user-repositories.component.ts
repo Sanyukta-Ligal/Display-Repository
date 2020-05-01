8c7e0d07499a57d2697a13d7f4dc5fe16a65fb67
Sanyukta Ligal <sanyuktaligal9999@gmail.com>
2020-05-01 23:20:11 +0545

    design changes

--- src/app/components/repositories/user-repositories/user-repositories.component.ts
+++ src/app/components/repositories/user-repositories/user-repositories.component.ts
@@ -12,7 +12,7 @@ import {
   catchError,
 
 } from 'rxjs/operators';
-import {Location} from  '@angular/common';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-user-repositories',
@@ -33,90 +33,85 @@ export class UserRepositoriesComponent implements OnInit {
   image;
   userName: string;
   selectedValue: any;
-  repoLists:any;
+  repoLists: any;
   filterLists = [{ value: 'All', name: 'All' }, { value: 'Fork', name: 'Fork' }, { value: 'Archived', name: 'Archived' }];
-spinning=true;
-  constructor(private _httpClient: HttpService,private _httpGitHubApiService:GitHubApiService,private _router:Router) { }
+  spinning = true;
+  constructor(private _httpClient: HttpService, private _httpGitHubApiService: GitHubApiService, private _router: Router) { }
 
   ngOnInit(): void {
-  
     let selectedValue = 'tpope';
+    this.getRepositories(selectedValue);
+  }
+
+  getRepositories(selectedValue: string) {
     this._httpGitHubApiService.getRepositories(selectedValue).subscribe((result: any) => {
       this.repositories = result;
-      this.spinning=false;
+      this.spinning = false;
       this.image = result[0].owner.avatar_url;
       this.userName = result[0].owner.login;
       this.totalItem = result.length;
-      this.repoLists=result;
+      this.repoLists = result;
+      this._router.navigate(["/repositories/" + `${selectedValue}`]);
     }
     );
   }
 
   modelValueChange($event) {
     let result;
-        let filterBy = $event.toLowerCase();
-        if(filterBy=="archived"){
-          result = this.repositories.filter(val => val.archived == true);
-          this.repositories=result;
-        }else if(filterBy=="fork"){
-          result = this.repositories.filter(val => val.fork == true);
-          this.repositories=result;
-        }else{
-          this.repositories=this.repoLists;
-        }
+    let filterBy = $event.toLowerCase();
+    if (filterBy == "archived") {
+      result = this.repositories.filter(val => val.archived == true);
+      this.repositories = result;
+    } else if (filterBy == "fork") {
+      result = this.repositories.filter(val => val.fork == true);
+      this.repositories = result;
+    } else {
+      this.repositories = this.repoLists;
+    }
+    this.totalItem = this.repositories.length
+  }
+
+  handleClick($event) {
+    let selectedValue
+    if (this.myControl.value.login) {
+      this.spinning = true;
+      if (this.myControl.value.login) {
+        selectedValue = this.myControl.value.login;
+      } else {
+        selectedValue = 'tpope';
       }
-    
-    
-      handleClick($event) {
-        
-        let selectedValue
-        if (this.myControl.value.login) {
-          this.spinning=true;
-          if (this.myControl.value.login) {
-            selectedValue = this.myControl.value.login;
-          } else {
-            selectedValue = 'tpope';
-          }
-          this._httpGitHubApiService.getRepositories(selectedValue).subscribe((result: any) => {
-            this.repositories = result;
-            this.spinning=false;
-            this.image = result[0].owner.avatar_url;
-            this.userName = result[0].owner.login;
-            this.totalItem = result.length;
-            this._router.navigate(["/repos/" + selectedValue])
-          }
-          );
-        }
-      }
-     
-      inputFormatterForProvider = (x: { login: string }) => x.login;
-    
-      searchUser = (text$: Observable<string>) =>
-        text$.pipe(
-          debounceTime(200),
-          distinctUntilChanged(),
-          tap(() => this.providerSearch
-            = true),
-          switchMap(term => {
-            if (term.length > 2) {
-              return this._httpGitHubApiService.searchUsers(term).pipe(map(a => {
-                this.providerSearch = false;
-                return a.items;
-              }
-              ),
-                tap(() => this.searchFailed = false),
-                catchError(() => {
-                  this.searchFailed = true;
-                  this.providerSearch = false;
-                  return of([]);
-                }));
-            } else {
-              this.providerSearch = false;
-              return of([]);
-            }
+      this.getRepositories(selectedValue);
+    }
+  }
+
+  inputFormatterForProvider = (x: { login: string }) => x.login;
+
+  searchUser = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      tap(() => this.providerSearch
+        = true),
+      switchMap(term => {
+        if (term.length > 2) {
+          return this._httpGitHubApiService.searchUsers(term).pipe(map(a => {
+            this.providerSearch = false;
+            return a.items;
           }
           ),
-          tap(() => this.providerSearch = false)
-        )
+            tap(() => this.searchFailed = false),
+            catchError(() => {
+              this.searchFailed = true;
+              this.providerSearch = false;
+              return of([]);
+            }));
+        } else {
+          this.providerSearch = false;
+          return of([]);
+        }
+      }
+      ),
+      tap(() => this.providerSearch = false)
+    )
 
 }
--- src/app/services/git-hub-api.service.ts
+++ src/app/services/git-hub-api.service.ts
@@ -4,6 +4,9 @@ import { map } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { environment } from '../../environments/environment';
 const API_BASE_URL = environment.gitRepoUrl;
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { HttpHeaders, HttpParams, HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable()
 export class GitHubApiService {
@@ -11,20 +14,36 @@ export class GitHubApiService {
         private http: HttpService
     ) { }
 
+
     searchUsers(term) {
         if (term && term.length > 2) {
-            return this.http.get(`/search/users?q=${term}`)
+            return this.http.get(`/search/users?q=${term}` + "&client_id=991a16711e7ae0843650&client_secret=6bdb59ab3a12c99d8acebea7713466cacf65cfc6")
                 .pipe(map((response: any) => {
                     console.log("response foruser", response);
                     return response;
-                }));
+                }),
+                    (
+                        catchError(this.handleError)
+                    ));
         } else {
             return of([]);
         }
     }
 
     getRepositories(selectedItem) {
-        let url = `/users/${selectedItem}/repos`;
-        return this.http.get(`${url}`);
+        let url = `/users/${selectedItem}/repos` + "?client_id=991a16711e7ae0843650&client_secret=6bdb59ab3a12c99d8acebea7713466cacf65cfc6";
+        return this.http.get(`${url}`).pipe(catchError(this.handleError));
+    }
+
+    private handleError(error: HttpErrorResponse) {
+        console.log('service handle error: ', error);
+        if (error.error) {
+            return throwError({ status: error.status, message: error.error.message });
+        } else {
+            return throwError({
+                status: 500, success: false,
+                message: `Backend returned code ${error.status} body was: ${error.message}`
+            })
+        }
     }
 }
